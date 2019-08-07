53a36872b12475ce4fe89370d46cdf1403384181
jaybenaim <benaimjacob@gmail.com>
2019-08-06 22:02:19 -0400

    added edit and delete for comment and article

--- blog_app/views.py
+++ blog_app/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login 
 from django.contrib.auth.forms import UserCreationForm 
 from django.contrib.auth import authenticate
+from django.urls import reverse 
 
 
 def root(request): 
@@ -61,7 +62,7 @@ def create_comment(request):
     comment.article = article
     comment.save() 
 
-    return redirect('/')
+    return redirect(reverse('post_details', args=[article.id]))
 
 @login_required 
 def new_article(request): 
@@ -101,6 +102,47 @@ def create_article(request):
     
     return render(request, 'new_article.html', context) 
 
-    
 
+@login_required 
+def edit_article(request, id): 
+    article = get_object_or_404(Article, id=id)
+    if request.method == 'GET': 
+        form = ArticleForm(instance=article)
+        context = {'form': form, 'article': article}
+        return render(request, 'edit_article.html', context) 
+
+    elif request.method == 'POST': 
+        form = ArticleForm(request.POST, instance=article) 
+        if form.is_valid():
+            update_article = form.save() 
+            return redirect(reverse('post_details', args=[article.id]))
+        else: 
+            context = {'form': form, 'article': article}
+            return render(request, 'edit_article.html', context)
+
+@login_required 
+def delete_article(request, id): 
+    article = get_object_or_404(Article, id=id)
+    article.delete() 
+    return redirect('/')
+
+@login_required 
+def edit_comment(request, id): 
+    comment = get_object_or_404(Comment, id=id)
+    if request.method == 'GET': 
+        form = CommentForm(instance=comment) 
+        context = { 
+            'form': form,
+            'comment': comment, 
+        }
+        return redirect(reverse('edit_comment', args=[comment.id]))
+    else: 
+        context = { 'form': form, 'comment': comment}
+        return render(request, 'edit_comment', context)
+
+@login_required
+def delete_comment(request, id): 
+    comment = get_object_or_404(Comment, id=id)
+    comment.delete()
+    return redirect(reverse('post_details', args=[comment.article.id]))
 
